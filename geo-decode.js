05a306878a873b3bfc2d7339ef5c4377fddaea65
Anita Lipsky <anita@purplebugs.com>
2022-09-09 13:32:49 +0200

    anonymise data, TODO for bugfix if returns > 1

--- geo-decode.js
+++ geo-decode.js
@@ -1,16 +1,16 @@
 import fetch from "node-fetch";
 
-// {"Zip":"1667","City":"Rolvsøy","Name":"Alpakka Enghaugen","Gender":"SEX_FEMALE","Keeper":16,"Street":"Ringstadhavna 7","color1":"COLOR_WHITE","Country":"NO","Webpage":"http://www.alpakkaenghaugen.no/","alpacaId":271,"DateOfBirth":"2004-05-13","DateOfDeath":null,"alpacaShortName":"ANDAMOOKA CONSTANTINE"}
-// {"Zip":"1667","City":"Rolvsøy","Street":"Ringstadhavna 7"}
+// {"Zip":"0167","City":"Oslo","Name":"My alpacca test farm","Gender":"SEX_FEMALE","Keeper":123,"Street":"Wergelandsveien 15","color1":"COLOR_WHITE","Country":"NO","Webpage":"http://www.myalpacatestfarm.com/","alpacaId":456,"DateOfBirth":"2008-05-13","DateOfDeath":null,"alpacaShortName":"ANITA"}
+// {"Zip":"0167","City":"Oslo","Street":"Wergelandsveien 15"}
 
 const addressJSON = {
-  Zip: "2436",
-  City: "Våler i Solør",
-  Street: "Risåsvegen 71",
-}; // Previously "Risåsvegen 71, 2436, Våler i Solør";
+  Zip: "0167",
+  City: "Oslo",
+  Street: "Wergelandsveien 15",
+};
 
 const addressJSONtoString = (address) => {
-  // { Zip: "2436", City: "Våler i Solør", Street: "Risåsvegen 71"} ->  "Risåsvegen 71, 2436, Våler i Solør"
+  // { Zip: "0167", City: "Oslo", Street: "Wergelandsveien 15"} ->  "Wergelandsveien 15, 0167, Oslo"
   return `${addressJSON["Street"]}, ${addressJSON["Zip"]}, ${addressJSON["City"]}`;
 };
 
@@ -21,7 +21,8 @@ const getLatLongFromGeoNorge = async (address) => {
 
   console.log(address);
 
-  // sokeStreng: 'sok=Ris%C3%A5svegen%2071,%202436,%20V%C3%A5ler%20i%20Sol%C3%B8r&fuzzy=true&utkoordsys=4258&treffPerSide=10&side=1'
+  // sokeStreng: "sok=Wergelandsveien%2015%2C%200167%2C%20Oslo&fuzzy=true&utkoordsys=4258&treffPerSide=10&side=1"
+  // TODO - investigate using kommunenavn= for when more than one result:  "sokeStreng": "sok=Hagegata%2028%2C%200653&fuzzy=true&kommunenavn=Oslo&utkoordsys=4258&treffPerSide=10&side=1"
 
   const response = await fetch(
     `https://ws.geonorge.no/stedsnavn/v1/sted?sok=${address}&fuzzy=true&utkoordsys=4258&treffPerSide=10&side=1`
@@ -32,7 +33,7 @@ const getLatLongFromGeoNorge = async (address) => {
 };
 
 getLatLongFromGeoNorge(addressJSONtoString(addressJSON)).then((data) =>
-  // Example response: { 'øst': 11.77152, nord: 60.68571, koordsys: 4258 }
+  // Example response: { 'øst': 7.72357, nord: 63.11076, koordsys: 4258 }
 
   console.log(data?.navn[0]?.representasjonspunkt)
 );
