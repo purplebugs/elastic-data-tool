dcb290a41c45794892375bbff4511a9bc1d225c3
Anita Lipsky <anita@purplebugs.com>
2022-09-11 21:06:03 +0200

    more precise search, handle encoding, null / undefined

--- geo-decode.js
+++ geo-decode.js
@@ -1,19 +1,27 @@
 import fetch from "node-fetch";
 
-const addressJSONtoString = (address) => {
-  // { Zip: "0167", City: "Oslo", Street: "Wergelandsveien 15"} ->  "Wergelandsveien 15, 0167, Oslo"
-  return `${address["Street"]}, ${address["Zip"]}, ${address["City"]}`;
-};
-
 const getLatLongFromGeoNorge = async (address) => {
+  // example address: { zip: "0167", city: "Oslo", street: "Wergelandsveien 15"} ->  "Wergelandsveien 15, 0167, Oslo"
   console.log(address);
+
+  if (!address || !address.street || !address.zip || !address.city) {
+    return;
+  }
+
+  const searchParams = new URLSearchParams();
+  searchParams.set("fuzzy", "false");
+  searchParams.set("adressetekst", address.street);
+  searchParams.set("postnummer", address.zip.toString());
+  searchParams.set("poststed", address.city);
+  searchParams.set("utkoordsys", "4258");
+  searchParams.set("treffPerSide", "10");
+  searchParams.set("side", "0");
+  searchParams.set("asciiKompatibel", "true");
 
   // Ref: https://kartkatalog.geonorge.no/metadata/adresse-rest-api/44eeffdc-6069-4000-a49b-2d6bfc59ac61
 
-  // TODO: Improve search using postnummer https://ws.geonorge.no/adresser/v1/sok?sok=Wergelandsveien%2015%2C%20Oslo&fuzzy=false&postnummer=0167&utkoordsys=4258&treffPerSide=10&side=0&asciiKompatibel=true
-
   const response = await fetch(
-    `https://ws.geonorge.no/adresser/v1/sok?sok=${address}&fuzzy=false&utkoordsys=4258&treffPerSide=10&side=0&asciiKompatibel=true`
+    `https://ws.geonorge.no/adresser/v1/sok?${searchParams}`
   );
   const data = await response.json();
 
@@ -21,7 +29,7 @@ const getLatLongFromGeoNorge = async (address) => {
 };
 
 export const printAddressToConsole = (address) => {
-  getLatLongFromGeoNorge(addressJSONtoString(address)).then((data) =>
+  getLatLongFromGeoNorge(address).then((data) =>
     // { epsg: 'EPSG:4258', lat: 59.919244748168225, lon: 10.731070562579866 }
 
     console.log(data?.adresser[0]?.representasjonspunkt)
--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const myOutput = [];
 const now = Date.now().toString();
 
 // Read file from disk
-const myFile = readFileSync("./data/alpacas-two-only.json");
+const myFile = readFileSync(
+  "./data/alpacas-address-camelCase-several-only.json"
+);
 
 // Parse file
 const myParsedFile = JSON.parse(myFile);
@@ -15,8 +17,8 @@ const myParsedFile = JSON.parse(myFile);
 myParsedFile.forEach((item, count) => {
   // convert each item to a JSON string, and conveniently stringify also removes spaces
   // eg
-  // {"Name":"Happiest alpaca farm","Street":"the street","alpaca":"Fluffy","Webpage":null,"alpacaId":123,"idOwners":2,"idCompany":3, Zip: "0167", City: "Oslo", Street: "Another Steet 132"}
-  // {"Name":"Cutest alpaca place","Street":"another street","alpaca":"Chanel","Webpage":null,"alpacaId":345,"idOwners":4,"idCompany":6, Zip: "0167", City: "Oslo", Street: "Wergelandsveien 15"}
+  // {"name":"Happiest alpaca farm","street":"the street","alpacaShortName":"Fluffy","webpage":null,"alpacaId":123,"idOwners":2,"idCompany":3, zip: "0167", city: "Oslo", street: "Another Steet 132"}
+  // {"name":"Cutest alpaca place","street":"another street","alpacaShortName":"Chanel","webpage":null,"alpacaId":345,"idOwners":4,"idCompany":6, zip: "0167", city: "Oslo", street: "Wergelandsveien 15"}
 
   printAddressToConsole(item); // Work in progress: add latitude, longitude based on item's address fields, TODO bugfix undefined from loop with async/await
 
