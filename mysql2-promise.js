3f7390a198f06c2d3ea66c496a3c8c63dfb89285
Anita Lipsky <anita@purplebugs.com>
2022-10-03 13:18:07 +0200

    mysql query with promise, proof of concept

---
+++ mysql2-promise.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+const config = dotenv.config();
+import pkg from "mysql2/promise";
+
+async function connectToDb() {
+  const mysqlPromise = pkg; // get the client
+  // create the connection
+  const connection = await mysqlPromise.createConnection({
+    host: "localhost",
+    user: "root",
+    password: process.env.MYSQL_PASSWORD,
+    database: "anita_database",
+  });
+
+  return connection;
+}
+
+async function queryDb(connection) {
+  // query database
+  const result = await connection.execute("SELECT * FROM `alp_Location`");
+
+  return result;
+}
+
+const connection = await connectToDb();
+//console.log(connection);
+
+const [results, fields] = await queryDb(connection);
+console.log("results", results);
+console.log("fields", fields);
