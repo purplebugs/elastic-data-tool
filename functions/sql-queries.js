c7f403944f083267d4538667c85da9599715bff3
Anita Lipsky <anita@purplebugs.com>
2022-10-03 14:27:11 +0200

    More queries

--- functions/sql-queries.js
+++ functions/sql-queries.js
@@ -30,3 +30,20 @@ export const getAlpacaIdsFromNorwegianRegistry = async (connection) => {
 
   return result;
 };
+
+export const getAlpacaDetails = async (connection) => {
+  const query =
+    "select a.idAlpaca as AlpacaId, a.ShortName AS AlpacaShortName, " +
+    "s.Name AS Gender, " +
+    "color1.Name AS AlpacaColor1, " +
+    "CONVERT(DOB, DATE) AS DOB, CONVERT(DOD, DATE) AS DOD, " +
+    "a.Keeper AS KeeperId, " +
+    "c.idCompany AS CompanyId, c.Name AS KeeperName, c.Street, c.Zip, c.City, c.Country, c.Webpage FROM `alp_Alpaca` a " +
+    "INNER JOIN alp_Sex s ON a.Sex = s.idSex " +
+    "INNER JOIN alp_Color color1 ON a.Color1 = color1.idColor " +
+    "INNER JOIN alp_Company c on a.Keeper = c.idCompany WHERE a.idAlpaca IN " +
+    "(SELECT r.Alpaca FROM alp_Register r INNER JOIN alp_Registry y ON r.Registry = y.idRegistry where r.Registry=1)";
+
+  const result = await connection.execute(query);
+  return result;
+};
--- sql-to-json.js
+++ sql-to-json.js
@@ -2,6 +2,7 @@ import {
   connectToDb,
   getAlpacaRegistries,
   getAlpacaIdsFromNorwegianRegistry,
+  getAlpacaDetails,
 } from "./functions/sql-queries.js";
 
 /******** SQL to JSON - in progress ********/
@@ -10,7 +11,11 @@ const connection = await connectToDb();
 const [alpacaRegistries] = await getAlpacaRegistries(connection);
 console.log("alpacaRegistries", alpacaRegistries);
 
-const [results] = await getAlpacaIdsFromNorwegianRegistry(connection);
-console.log("results", results);
+const [alpacaIdsFromNorwegianRegistry] =
+  await getAlpacaIdsFromNorwegianRegistry(connection);
+console.log("alpacaIdsFromNorwegianRegistry", alpacaIdsFromNorwegianRegistry);
+
+const [alpacaDetails] = await getAlpacaDetails(connection);
+console.log("alpacaDetails", alpacaDetails);
 
 // TODO convert SQL query result to JSON and store in file
