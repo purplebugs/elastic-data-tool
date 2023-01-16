9b3c5843f11293126572f474a87eed004eb338a0
Anita Lipsky <anita@purplebugs.com>
2023-01-16 21:15:42 +0100

    file transformer in separate function

---
+++ functions/fileTransformer.js
@@ -0,0 +1,34 @@
+import { getLatLongFromGeoNorge } from "./geo-decode.js";
+import { populationByMunicipalityLookup } from "./geo-enrich/population-by-municipality.js";
+
+export default async function fileTransformer(file) {
+  // Loop over all items
+
+  const myOutput = [];
+  let count = 1;
+
+  for await (const item of file) {
+    // convert each item to a JSON string
+    // {"name":"Happiest alpaca farm","street":"the street","alpacaShortName":"Fluffy","webpage":null,"alpacaId":123,"idOwners":2,"idCompany":3, zip: "0167", city: "Oslo", street: "Another Steet 132"}
+    // {"name":"Cutest alpaca place","street":"another street","alpacaShortName":"Chanel","webpage":null,"alpacaId":345,"idOwners":4,"idCompany":6, zip: "0167", city: "Oslo", street: "Wergelandsveien 15"}
+
+    // Label all farms as private for import into Elasticsearch
+    // because public facing farm index will be created in Elasticsearch based on query and pipeline
+    const publicFieldAdded = Object.assign({}, item, { public: false });
+
+    const geoDecodeObj = await getLatLongFromGeoNorge(item);
+    const geoDecodedObj = Object.assign(publicFieldAdded, geoDecodeObj);
+
+    const geoEnrichObj = populationByMunicipalityLookup(geoDecodedObj);
+    const geoEnrichedObj = Object.assign(geoDecodedObj, geoEnrichObj);
+
+    // Uncomment for Elasticsearch POST /_bulk body format
+    // myOutput.push(JSON.stringify({ index: { _id: count } }));
+
+    // conveniently stringify also removes spaces
+    myOutput.push(JSON.stringify(geoEnrichedObj));
+
+    count++;
+  }
+  return myOutput;
+}
--- index.js
+++ index.js
@@ -1,45 +1,16 @@
 import { writeFileSync } from "fs";
 import fileReader from "./functions/fileReader.js";
-import { getLatLongFromGeoNorge } from "./functions/geo-decode.js";
-import { populationByMunicipalityLookup } from "./functions/geo-enrich/population-by-municipality.js";
+import fileTransformer from "./functions/fileTransformer.js";
 
 /******** JSON to NDJSON and geodecode, enrich ********/
 
 const now = Date.now().toString();
-const myOutput = [];
 
 // Edit this file name as needed
 const jsonFileToConvertToNDJSON = "alpacas-address-camelCase-several-only.json";
 
 const myParsedFile = fileReader(jsonFileToConvertToNDJSON);
-
-// Loop over all items
-
-let count = 1;
-
-for await (const item of myParsedFile) {
-  // convert each item to a JSON string
-  // {"name":"Happiest alpaca farm","street":"the street","alpacaShortName":"Fluffy","webpage":null,"alpacaId":123,"idOwners":2,"idCompany":3, zip: "0167", city: "Oslo", street: "Another Steet 132"}
-  // {"name":"Cutest alpaca place","street":"another street","alpacaShortName":"Chanel","webpage":null,"alpacaId":345,"idOwners":4,"idCompany":6, zip: "0167", city: "Oslo", street: "Wergelandsveien 15"}
-
-  // Label all farms as private for import into Elasticsearch
-  // because public facing farm index will be created in Elasticsearch based on query and pipeline
-  const publicFieldAdded = Object.assign({}, item, { public: false });
-
-  const geoDecodeObj = await getLatLongFromGeoNorge(item);
-  const geoDecodedObj = Object.assign(publicFieldAdded, geoDecodeObj);
-
-  const geoEnrichObj = populationByMunicipalityLookup(geoDecodedObj);
-  const geoEnrichedObj = Object.assign(geoDecodedObj, geoEnrichObj);
-
-  // Uncomment for Elasticsearch POST /_bulk body format
-  // myOutput.push(JSON.stringify({ index: { _id: count } }));
-
-  // conveniently stringify also removes spaces
-  myOutput.push(JSON.stringify(geoEnrichedObj));
-
-  count++;
-}
+const myOutput = await fileTransformer(myParsedFile);
 
 // joining all items in the array with new lines to form NDJSON
 const myOutputFileContents = myOutput.join("\n");
