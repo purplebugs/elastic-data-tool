8f41f6bdfedad3825a28657a9508f6988cd821bc
Anita Lipsky <anita@purplebugs.com>
2023-01-23 13:56:04 +0100

    Local db connection by default

--- functions/sql_queries/connect_to_db.js
+++ functions/sql_queries/connect_to_db.js
@@ -2,15 +2,19 @@ import dotenv from "dotenv";
 const config = dotenv.config();
 import pkg from "mysql2/promise";
 
-export const connectToDb = async () => {
-  const mysqlPromise = pkg; // get the client
-  // create the connection
-  const connection = await mysqlPromise.createConnection({
-    host: "localhost",
-    user: "root",
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-  });
+export const connectToDb = async (cloud = false) => {
+  let connection = null;
+
+  if (!cloud) {
+    const mysqlPromise = pkg; // get the client
+    // create the connection
+    connection = await mysqlPromise.createConnection({
+      host: "localhost",
+      user: "root",
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+    });
+  }
 
   return connection;
 };
