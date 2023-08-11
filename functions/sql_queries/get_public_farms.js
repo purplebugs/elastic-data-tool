335aa66e292a9a26c0a9ae64c4d3095759b5f0cd
Anita Lipsky <anita@purplebugs.com>
2023-08-11 10:54:43 +0200

    fix: Format public farms, query

--- functions/sql_queries/get_public_farms.js
+++ functions/sql_queries/get_public_farms.js
@@ -1,9 +1,12 @@
 import { PUBLIC_FARMS } from "./public_farms.js";
 
+const publicFarms = PUBLIC_FARMS.map((farm) => {
+  return `"${farm}"`;
+});
+
 export const getPublicFarms = async (connection) => {
   // query database
-  const query =
-    "SELECT idCompany AS keeper, Name AS name FROM `alp_Company` WHERE `Name` IN (" + PUBLIC_FARMS + ") ORDER BY Name";
+  const query = `SELECT idCompany AS keeper, Name AS name FROM alp_Company WHERE Name IN (${publicFarms}) ORDER BY Name`;
   const result = await connection.execute(query);
 
   return result;
