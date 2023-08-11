2c8255a4ee447696990b8a0e682df74d98020687
Anita Lipsky <anita@purplebugs.com>
2023-08-11 11:35:45 +0200

    feat: Label farm as public in index. No longer need to do this in Elasticsearch with enrich policy.

--- functions/fileTransformer.js
+++ functions/fileTransformer.js
@@ -1,5 +1,6 @@
 import { getLatLongFromGeoNorge } from "./geo-decode.js";
 import { populationByMunicipalityLookup } from "./geo-enrich/population-by-municipality.js";
+import { PUBLIC_FARMS } from "./sql_queries/public_farms.js";
 
 export default async function fileTransformer(file, bulkSyntax) {
   // Loop over all items
@@ -12,9 +13,12 @@ export default async function fileTransformer(file, bulkSyntax) {
     // {"name":"Happiest alpaca farm","street":"the street","alpacaShortName":"Fluffy","webpage":null,"alpacaId":123,"idOwners":2,"idCompany":3, zip: "0167", city: "Oslo", street: "Another Steet 132"}
     // {"name":"Cutest alpaca place","street":"another street","alpacaShortName":"Chanel","webpage":null,"alpacaId":345,"idOwners":4,"idCompany":6, zip: "0167", city: "Oslo", street: "Wergelandsveien 15"}
 
-    // Label all farms as private for import into Elasticsearch
-    // because public facing farm index will be created in Elasticsearch based on query and pipeline
-    const publicFieldAdded = Object.assign({}, item, { public: false });
+    // Label farm as public if in approved list.  No longer needs to be done in Elasticsearch using enrich policy
+    let publicFieldAdded = Object.assign({}, item, { public: false });
+    if (item.keeperName && PUBLIC_FARMS.some((element) => element.toLowerCase() === item.keeperName.toLowerCase())) {
+      publicFieldAdded = Object.assign({}, item, { public: true });
+      console.log("[LOG] Farm is public: ", item.keeperName);
+    }
 
     const geoDecodeObj = await getLatLongFromGeoNorge(item);
     const geoDecodedObj = Object.assign(publicFieldAdded, geoDecodeObj);
