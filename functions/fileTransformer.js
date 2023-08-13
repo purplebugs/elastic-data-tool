b665528c44a3aae3976466b6c36ab145c75576e7
Anita Lipsky <anita@purplebugs.com>
2023-08-13 19:04:40 +0200

    test: File transformer adds public flag correctly. Bugfix fileTransformer for no geoDecodeEnrich

--- functions/fileTransformer.js
+++ functions/fileTransformer.js
@@ -14,33 +14,30 @@ export default async function fileTransformer(file, { bulkSyntax = false }, { ge
     // {"name":"Cutest alpaca place","street":"another street","alpacaShortName":"Chanel","webpage":null,"alpacaId":345,"idOwners":4,"idCompany":6, zip: "0167", city: "Oslo", street: "Wergelandsveien 15"}
 
     // Label farm as public if in approved list.  No longer needs to be done in Elasticsearch using enrich policy
-    let publicFieldAdded = Object.assign({}, item, { public: false });
+    let itemTransformed = Object.assign({}, item, { public: false });
+
     if (item.keeperName && PUBLIC_FARMS.some((element) => element.toLowerCase() === item.keeperName.toLowerCase())) {
-      publicFieldAdded = Object.assign({}, item, { public: true });
+      itemTransformed = Object.assign({}, item, { public: true });
       console.log("[LOG] Farm is public: ", item.keeperName);
     }
 
-    let geoEnrichedObj = {};
     if (geoDecodeEnrich) {
       const geoDecodeObj = await getLatLongFromGeoNorge(item);
-      const geoDecodedObj = Object.assign(publicFieldAdded, geoDecodeObj);
+      itemTransformed = Object.assign(itemTransformed, geoDecodeObj);
 
-      const geoEnrichObj = populationByMunicipalityLookup(geoDecodedObj);
-      geoEnrichedObj = Object.assign(geoDecodedObj, geoEnrichObj);
+      const geoEnrichObj = populationByMunicipalityLookup(itemTransformed);
+      itemTransformed = Object.assign(itemTransformed, geoEnrichObj);
     }
 
-    if (bulkSyntax && geoDecodeEnrich) {
+    if (bulkSyntax) {
       // For Elasticsearch POST /_bulk body format
       //[{ create: {} }, alpacaDocument_1, { create: {} }, alpacaDocument_2],
-
       myOutput.push({ create: {} });
-      myOutput.push(geoEnrichedObj);
     }
 
-    if (!bulkSyntax && geoDecodeEnrich) {
-      // conveniently stringify also removes spaces
-      myOutput.push(JSON.stringify(geoEnrichedObj));
-    }
+    // conveniently stringify also removes spaces
+    myOutput.push(JSON.stringify(itemTransformed));
+
     //console.log(myOutput);
 
     count++;
--- test/functions/fileTransformer.js
+++ test/functions/fileTransformer.js
@@ -1,12 +1,55 @@
 import { describe, it } from "node:test";
-import assert from "node:assert";
+import { strict as assert } from "node:assert";
+import fileTransformer from "../../functions/fileTransformer.js";
 
-describe("A thing", () => {
-  it("should work", () => {
-    assert.strictEqual(1, 1);
+describe("Farm info transformer", async () => {
+  it("should NOT add public farm info if farm NOT in list of public farms", async () => {
+    // ARRANGE
+    const alpacaDetailsArray = [
+      {
+        alpacaId: 1234,
+        keeperName: "Not a public farm name",
+      },
+    ];
+
+    // ACT
+    const result = await fileTransformer(alpacaDetailsArray, { bulkSyntax: false }, { geoDecodeEnrich: false });
+
+    console.log("result[0]", result[0]);
+
+    // ASSERT
+    assert.equal(
+      result[0],
+      JSON.stringify({
+        alpacaId: 1234,
+        keeperName: "Not a public farm name",
+        public: false,
+      })
+    );
   });
 
-  it("should fail", () => {
-    assert.strictEqual(2, 3);
+  it("should add public farm info if farm IS in list of public farms", async () => {
+    // ARRANGE
+    const alpacaDetailsArray = [
+      {
+        alpacaId: 1234,
+        keeperName: "Alpakkahagen",
+      },
+    ];
+
+    // ACT
+    const result = await fileTransformer(alpacaDetailsArray, { bulkSyntax: false }, { geoDecodeEnrich: false });
+
+    console.log("result[0]", result[0]);
+
+    // ASSERT
+    assert.equal(
+      result[0],
+      JSON.stringify({
+        alpacaId: 1234,
+        keeperName: "Alpakkahagen",
+        public: true,
+      })
+    );
   });
 });
