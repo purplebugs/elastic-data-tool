4e6e16f6fb7f4d1bc0c19089db2a3537f4f77f3b
Anita Lipsky <anita@purplebugs.com>
2023-08-14 11:47:26 +0200

    test: Expand test

--- test/get_public_farms.js
+++ test/get_public_farms.js
@@ -5,7 +5,10 @@ import { transformerBulkSyntax } from "../get_public_farms.js";
 describe("Get public farms", async () => {
   it("should transform public farms with elasticsearch _bulk syntax", async () => {
     // ARRANGE
-    const publicFarmArray = [{ keeper: 1234, name: "Alpakkahagen" }];
+    const publicFarmArray = [
+      { keeper: 1234, name: "Alpakkahagen" },
+      { keeper: 5678, name: "Another public farm" },
+    ];
 
     // ACT
     const result = await transformerBulkSyntax(publicFarmArray);
@@ -13,6 +16,9 @@ describe("Get public farms", async () => {
     console.log("result", result);
     // ASSERT
 
-    assert.equal(result, `{"index":{"_id":1}}\n{"keeper":1234,"name":"Alpakkahagen","public":true}`);
+    assert.equal(
+      result,
+      `{"index":{"_id":1}}\n{"keeper":1234,"name":"Alpakkahagen","public":true}\n{"index":{"_id":2}}\n{"keeper":5678,"name":"Another public farm","public":true}`
+    );
   });
 });
