24d01d9ffed07ec564c5886079a036919cc77347
Anita Lipsky <anita@purplebugs.com>
2023-08-14 11:41:32 +0200

    test: Transform public farms with elasticsearch bulkSyntax

--- get_public_farms.js
+++ get_public_farms.js
@@ -5,42 +5,29 @@ import { getPublicFarms } from "./functions/sql_queries/get_public_farms.js";
 import { writeFileSync } from "fs";
 
 const now = Date.now().toString();
-const myOutput = [];
 
-/******** SQL to JSON ********/
+export const transformerBulkSyntax = async (farms) => {
+  // For creation of public farms index in Elasticsearch to be used as enrich pipeline
 
-const connection = await connectToDb();
+  const myOutput = [];
+  let count = 1;
 
-const [publicFarms] = await getPublicFarms(connection);
-console.log("publicFarms", publicFarms);
+  for await (const item of farms) {
+    // For Elasticsearch POST /_bulk body format
+    myOutput.push(JSON.stringify({ index: { _id: count } }));
 
-const resultJSON = JSON.stringify(publicFarms);
+    // Label all farms as public
+    const publicFieldAdded = Object.assign({}, item, { public: true });
 
-// Write to file which will write as one long line
-writeFileSync(`./data/public-farms-from-sql-${now}.json`, resultJSON);
+    // conveniently stringify also removes spaces
+    myOutput.push(JSON.stringify(publicFieldAdded));
+    count++;
+  }
 
-/******** to NDJSON file ********/
+  // joining all items in the array with new lines to form NDJSON
+  const result = myOutput.join("\n");
 
-// For creation of public farms index in Elasticsearch to be used as enrich pipeline
-
-let count = 1;
-
-for await (const item of publicFarms) {
-  // For Elasticsearch POST /_bulk body format
-  myOutput.push(JSON.stringify({ index: { _id: count } }));
-
-  // Label all farms as public
-  const publicFieldAdded = Object.assign({}, item, { public: true });
-
-  // conveniently stringify also removes spaces
-  myOutput.push(JSON.stringify(publicFieldAdded));
-  count++;
-}
-
-// joining all items in the array with new lines to form NDJSON
-const myOutputFileContents = myOutput.join("\n");
-
-/* Example file output
+  /* Example file output
 
 {"index":{"_id":1}}
 {"keeper":32,"name":"Farm name 1","public":true}
@@ -49,7 +36,25 @@ const myOutputFileContents = myOutput.join("\n");
 
 */
 
-writeFileSync(
-  `./data/public-farms-for-elasticsearch-index-${now}.ndjson`,
-  myOutputFileContents
-);
+  return result;
+};
+
+/******** SQL to JSON ********/
+
+const connection = await connectToDb();
+
+const [publicFarms] = await getPublicFarms(connection);
+console.log("[LOG] Number of publicFarms: ", publicFarms.length);
+
+const resultJSON = JSON.stringify(publicFarms);
+
+// Write to file which will write as one long line
+writeFileSync(`./data/public-farms-from-sql-${now}.json`, resultJSON);
+console.log("[LOG] See file: ", `./data/public-farms-from-sql-${now}.json`);
+
+/******** to NDJSON file ********/
+
+const myOutputFileContents = await transformerBulkSyntax(publicFarms);
+
+writeFileSync(`./data/public-farms-for-elasticsearch-index-${now}.ndjson`, myOutputFileContents);
+console.log("[LOG] See file: ", `./data/public-farms-for-elasticsearch-index-${now}.ndjson`);
---
+++ test/get_public_farms.js
@@ -0,0 +1,18 @@
+import { describe, it } from "node:test";
+import { strict as assert } from "node:assert";
+import { transformerBulkSyntax } from "../get_public_farms.js";
+
+describe("Get public farms", async () => {
+  it("should transform public farms with elasticsearch _bulk syntax", async () => {
+    // ARRANGE
+    const publicFarmArray = [{ keeper: 1234, name: "Alpakkahagen" }];
+
+    // ACT
+    const result = await transformerBulkSyntax(publicFarmArray);
+
+    console.log("result", result);
+    // ASSERT
+
+    assert.equal(result, `{"index":{"_id":1}}\n{"keeper":1234,"name":"Alpakkahagen","public":true}`);
+  });
+});
