7c4743bef35fd74ba0b4fcdd16accf6a51873222
Anita Lipsky <anita@purplebugs.com>
2023-08-15 13:55:11 +0200

    refactor: Actual line breaks in test, more readable?

--- test/functions/publicFarmBulkSyntax.js
+++ test/functions/publicFarmBulkSyntax.js
@@ -16,7 +16,11 @@ describe("Get public farms", async () => {
     console.log("result", result);
     // ASSERT
 
-    const expected = `{"index":{"_id":1}}\n{"keeper":1234,"name":"Alpakkahagen","public":true}\n{"index":{"_id":2}}\n{"keeper":5678,"name":"Another public farm","public":true}`;
+    const expected = `{"index":{"_id":1}}
+{"keeper":1234,"name":"Alpakkahagen","public":true}
+{"index":{"_id":2}}
+{"keeper":5678,"name":"Another public farm","public":true}`;
+
     assert.equal(result, expected);
   });
 });
