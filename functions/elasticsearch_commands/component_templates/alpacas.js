dc697e2629ca34f04bfba56fdb1f751ea1aae1dd
Anita Lipsky <anita@purplebugs.com>
2023-08-17 21:05:10 +0200

    fix: Pass in componentTemplate as param

---
+++ functions/elasticsearch_commands/component_templates/alpacas.js
@@ -0,0 +1,112 @@
+export default {
+  create: false, // Replace/update existing. Ref: https://www.elastic.co/guide/en/elasticsearch/reference/8.9/indices-component-template.html#put-component-template-api-query-params
+  template: {
+    mappings: {
+      properties: {
+        alpacaId: {
+          type: "long",
+        },
+        alpacaShortName: {
+          type: "text",
+          fields: {
+            keyword: {
+              type: "keyword",
+              ignore_above: 256,
+            },
+          },
+        },
+        city: {
+          type: "text",
+          fields: {
+            keyword: {
+              type: "keyword",
+              ignore_above: 256,
+            },
+          },
+        },
+        color1: {
+          type: "text",
+          fields: {
+            keyword: {
+              type: "keyword",
+              ignore_above: 256,
+            },
+          },
+        },
+        country: {
+          type: "text",
+          fields: {
+            keyword: {
+              type: "keyword",
+              ignore_above: 256,
+            },
+          },
+        },
+        dateOfBirth: {
+          type: "date",
+        },
+        dateOfDeath: {
+          type: "date",
+        },
+        gender: {
+          type: "text",
+          fields: {
+            keyword: {
+              type: "keyword",
+              ignore_above: 256,
+            },
+          },
+        },
+        keeper: {
+          type: "long",
+        },
+        location: {
+          properties: {
+            coordinates: {
+              type: "geo_point",
+            },
+          },
+        },
+        name: {
+          type: "text",
+          fields: {
+            keyword: {
+              type: "keyword",
+              ignore_above: 256,
+            },
+          },
+        },
+        street: {
+          type: "text",
+          fields: {
+            keyword: {
+              type: "keyword",
+              ignore_above: 256,
+            },
+          },
+        },
+        webpage: {
+          type: "text",
+          fields: {
+            keyword: {
+              type: "keyword",
+              ignore_above: 256,
+            },
+          },
+        },
+        zip: {
+          type: "text",
+          fields: {
+            keyword: {
+              type: "keyword",
+              ignore_above: 256,
+            },
+          },
+        },
+      },
+    },
+  },
+  _meta: {
+    description: "Define JSON structure of document",
+  },
+};
--- functions/elasticsearch_commands/index.js
+++ functions/elasticsearch_commands/index.js
@@ -12,119 +12,6 @@ const client = new Client({
   },
 });
 
-const componentTemplate = {
-  create: false, // Replace/update existing. Ref: https://www.elastic.co/guide/en/elasticsearch/reference/8.9/indices-component-template.html#put-component-template-api-query-params
-  template: {
-    mappings: {
-      properties: {
-        alpacaId: {
-          type: "long",
-        },
-        alpacaShortName: {
-          type: "text",
-          fields: {
-            keyword: {
-              type: "keyword",
-              ignore_above: 256,
-            },
-          },
-        },
-        city: {
-          type: "text",
-          fields: {
-            keyword: {
-              type: "keyword",
-              ignore_above: 256,
-            },
-          },
-        },
-        color1: {
-          type: "text",
-          fields: {
-            keyword: {
-              type: "keyword",
-              ignore_above: 256,
-            },
-          },
-        },
-        country: {
-          type: "text",
-          fields: {
-            keyword: {
-              type: "keyword",
-              ignore_above: 256,
-            },
-          },
-        },
-        dateOfBirth: {
-          type: "date",
-        },
-        dateOfDeath: {
-          type: "date",
-        },
-        gender: {
-          type: "text",
-          fields: {
-            keyword: {
-              type: "keyword",
-              ignore_above: 256,
-            },
-          },
-        },
-        keeper: {
-          type: "long",
-        },
-        location: {
-          properties: {
-            coordinates: {
-              type: "geo_point",
-            },
-          },
-        },
-        name: {
-          type: "text",
-          fields: {
-            keyword: {
-              type: "keyword",
-              ignore_above: 256,
-            },
-          },
-        },
-        street: {
-          type: "text",
-          fields: {
-            keyword: {
-              type: "keyword",
-              ignore_above: 256,
-            },
-          },
-        },
-        webpage: {
-          type: "text",
-          fields: {
-            keyword: {
-              type: "keyword",
-              ignore_above: 256,
-            },
-          },
-        },
-        zip: {
-          type: "text",
-          fields: {
-            keyword: {
-              type: "keyword",
-              ignore_above: 256,
-            },
-          },
-        },
-      },
-    },
-  },
-  _meta: {
-    description: "Define JSON structure of document",
-  },
-};
-
 const createIndexName = (indexName) => {
   try {
     const addLeadingZero = (number) => (number < 10 ? `0${number}` : number);
@@ -141,7 +28,7 @@ const createIndexName = (indexName) => {
   }
 };
 
-const createComponentTemplate = async (componentTemplateName) => {
+const createComponentTemplate = async (componentTemplateName, componentTemplate) => {
   try {
     console.log(`[LOG] 🤖 Start of create component template: ${componentTemplateName}`);
 
@@ -230,7 +117,7 @@ const switchAlias = async (newIndexName, indexName) => {
   }
 };
 
-export default async function createIndexWithDocuments(indexName, items) {
+export default async function createIndexWithDocuments(indexName, items, componentTemplate) {
   try {
     const componentTemplateName = `${indexName}_component_template`;
     const indexTemplateName = `${indexName}_index_template`;
@@ -250,7 +137,7 @@ export default async function createIndexWithDocuments(indexName, items) {
     };
 
     const indexNameWithTimestamp = createIndexName(indexName);
-    await createComponentTemplate(componentTemplateName);
+    await createComponentTemplate(componentTemplateName, componentTemplate);
     await createIndexTemplate(indexTemplateName, indexTemplate);
 
     const resultCreateIndex = await client.bulk({
