5a21305feb8e5930eb323f411d6c8bd33bb38e89
Anita Lipsky <anita@purplebugs.com>
2023-08-18 10:22:54 +0200

    feat: Separate bulkSyntax function, not yet used

---
+++ functions/elasticsearch_commands/bulkSyntax.js
@@ -0,0 +1,10 @@
+export default bulkSyntax = (items) => {
+  // For Elasticsearch POST /_bulk body format, eg
+  //[{ create: {} }, alpacaDocument_1, { create: {} }, alpacaDocument_2],
+  const result = [];
+  items.map((item) => {
+    result.push({ create: {} });
+    result.push(item);
+  });
+  return result;
+};
