9b994201769a86f7320e71b5d97864a4056cd692
Anita Lipsky <anita@purplebugs.com>
2023-08-18 15:47:53 +0200

    fix: Error handling bulkSyntax()

--- functions/elasticsearch_commands/bulkSyntax.js
+++ functions/elasticsearch_commands/bulkSyntax.js
@@ -1,10 +1,15 @@
 export default function bulkSyntax(items) {
-  // For Elasticsearch POST /_bulk body format, eg
-  //[{ create: {} }, alpacaDocument_1, { create: {} }, alpacaDocument_2],
-  const result = [];
-  items.map((item) => {
-    result.push({ create: {} });
-    result.push(item);
-  });
-  return result;
+  try {
+    // For Elasticsearch POST /_bulk body format, eg
+    // [{ create: {} }, alpacaDocument_1, { create: {} }, alpacaDocument_2]
+    const result = [];
+    items.map((item) => {
+      result.push({ create: {} });
+      result.push(item);
+    });
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw new Error("🧨 bulkSyntax: Could not create Elasticsearch _bulk syntax");
+  }
 }
