c86c5673c54c7fe3e687371f2bbbc089f662be7c
Anita Lipsky <anita@purplebugs.com>
2023-08-18 15:50:52 +0200

    test: Simplify

--- test/functions/bulkSyntax.js
+++ test/functions/bulkSyntax.js
@@ -1,28 +1,35 @@
 import { describe, it } from "node:test";
 import { strict as assert } from "node:assert";
-import fileTransformer from "../../functions/fileTransformer.js";
 import bulkSyntax from "../../functions/elasticsearch_commands/bulkSyntax.js";
 
 describe("Elasticsearch _bulk syntax transformer", async () => {
-  it("should format transformed list with elasticsearch bulkSyntax()", async () => {
+  it("should format list with elasticsearch bulkSyntax()", async () => {
     // ARRANGE
-    const alpacaDetailsArray = [
+    const items = [
       {
-        alpacaId: 1234,
-        keeperName: "Not a public farm name",
+        id: 1,
+        name: "Name one",
+      },
+      {
+        id: 2,
+        name: "Name two",
       },
     ];
 
     // ACT
-    const result = bulkSyntax(await fileTransformer(alpacaDetailsArray, { geoDecodeEnrich: false }));
+    const result = bulkSyntax(items);
 
     // ASSERT
     assert.deepEqual(result, [
       { create: {} },
       {
-        alpacaId: 1234,
-        keeperName: "Not a public farm name",
-        public: false,
+        id: 1,
+        name: "Name one",
+      },
+      { create: {} },
+      {
+        id: 2,
+        name: "Name two",
       },
     ]);
   });
