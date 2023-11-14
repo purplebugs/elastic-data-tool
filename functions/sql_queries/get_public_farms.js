9eaef9effd13d05a03a6cebca5c568691f48eb4b
Anita Lipsky <anita@purplebugs.com>
2023-11-14 22:28:35 +0100

    feat: Get public farms from new alp_CompanyCategories table

--- functions/sql_queries/get_public_farms.js
+++ functions/sql_queries/get_public_farms.js
@@ -6,7 +6,7 @@ const publicFarms = PUBLIC_FARMS.map((farm) => {
 
 export const getPublicFarms = async (connection) => {
   // query database
-  const query = `SELECT idCompany AS keeper, Name AS name FROM alp_Company WHERE Name IN (${publicFarms}) ORDER BY Name`;
+  const query = `SELECT idCompany AS keeper, Name AS name FROM alp_Company WHERE idCompany IN (SELECT idCompanyCategory from alp_CompanyCategories where Category = 1) ORDER BY Name`;
   const result = await connection.execute(query);
 
   return result;
