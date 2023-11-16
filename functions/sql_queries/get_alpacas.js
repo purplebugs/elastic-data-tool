c45da83c36beb849a16ed94521c194ae11f7fc6a
Anita Lipsky <anita@purplebugs.com>
2023-11-16 10:58:54 +0100

    refactor: Order

--- functions/sql_queries/get_alpacas.js
+++ functions/sql_queries/get_alpacas.js
@@ -18,9 +18,9 @@ export const getAlpacaDetails = async (connection) => {
   const query =
     "select a.idAlpaca as alpacaId, a.RegisteredName AS alpacaRegisteredName, a.ShortName AS alpacaShortName, " +
     "s.Name AS gender, " +
+    "CONVERT(DOB, DATE) AS DOB, CONVERT(DOD, DATE) AS DOD, " +
     "b.Name AS breed, " +
     "color1.Name AS alpacaColor1, " +
-    "CONVERT(DOB, DATE) AS DOB, CONVERT(DOD, DATE) AS DOD, " +
     "a.Keeper AS keeper, c.idCompany AS companyId, c.Name AS keeperName, c.Street AS street, c.Zip AS zip, c.City AS city, c.Country as country, c.Webpage FROM `alp_Alpaca` a " +
     "INNER JOIN alp_Sex s ON a.Sex = s.idSex " +
     "INNER JOIN alp_Breed b ON a.Breed = b.idBreed " +
