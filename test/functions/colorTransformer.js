60e0a72c78c52773963b535e09e31b391f26c5b6
Anita Lipsky <anita@purplebugs.com>
2023-12-17 16:23:13 +0100

    test: Tests for null, some null, and all values

--- test/functions/colorTransformer.js
+++ test/functions/colorTransformer.js
@@ -3,17 +3,29 @@ import { strict as assert } from "node:assert";
 import { colorTransformer } from "../../functions/colorTransformer.js";
 
 describe("Color transformer", async () => {
-  it("should transform color properties to one color object with original and pretty names", async () => {
-    // TODO colours in all
-    // TODO test will all null
-    // TODO test with some null, missing
+  it("should transform color properties to null - all null or missing", async () => {
+    // ARRANGE
+    const object = {
+      color1: null,
+      color2: null,
+      colorSolid: null,
+    };
 
+    // ACT
+    const actual = colorTransformer(object);
+
+    // ASSERT
+    const expected = null;
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it("should transform color properties to one color object with null properties - some null or missing", async () => {
     // ARRANGE
     const object = {
       color1: "COLOR_LIGHT_FAWN",
-      color2: null,
       color3: null,
-      colorSolid: null,
+      colorSolid: "COLOR_BAY_BLACK",
     };
 
     // ACT
@@ -25,7 +37,32 @@ describe("Color transformer", async () => {
         color1: { original: "COLOR_LIGHT_FAWN" },
         color2: { original: null },
         color3: { original: null },
-        colorSolid: { original: null },
+        colorSolid: { original: "COLOR_BAY_BLACK" },
+      },
+    };
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it("should transform color properties to one color object with original and pretty names - all values", async () => {
+    // ARRANGE
+    const object = {
+      color1: "COLOR_LIGHT_FAWN",
+      color2: "COLOR_WHITE",
+      color3: "COLOR_MEDIUM_FAWN",
+      colorSolid: "COLOR_BAY_BLACK",
+    };
+
+    // ACT
+    const actual = colorTransformer(object);
+
+    // ASSERT
+    const expected = {
+      color: {
+        color1: { original: "COLOR_LIGHT_FAWN" },
+        color2: { original: "COLOR_WHITE" },
+        color3: { original: "COLOR_MEDIUM_FAWN" },
+        colorSolid: { original: "COLOR_BAY_BLACK" },
       },
     };
 
