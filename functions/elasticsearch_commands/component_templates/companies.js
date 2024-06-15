9174bb602f6cd0580bd1404f7d19aae6ba1cb5b3
Anita Lipsky <anita.lipsky@noaignite.com>
2024-06-15 16:30:45 +0200

    fix: Companies index location - type geo_point

--- functions/elasticsearch_commands/component_templates/companies.js
+++ functions/elasticsearch_commands/component_templates/companies.js
@@ -1230,7 +1230,7 @@ export default {
         location: {
           properties: {
             coordinates: {
-              type: "float",
+              type: "geo_point",
             },
             google: {
               properties: {
